034e4f684504c4a87bea9e6329ad67fead6dd791
zhoiton <k_volodiev@abv.bg>
2018-06-20 20:02:03 +0200

    -added menu on the members list in the company view

--- src/views/Company/CompanyMembers.js
+++ src/views/Company/CompanyMembers.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Avatar from "@material-ui/core/Avatar";
 import ListItemText from "@material-ui/core/ListItemText";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 export default class CompanyMembers extends Component {
 	static propTypes = {
@@ -15,19 +17,22 @@ export default class CompanyMembers extends Component {
 		super(props);
 		this.state = {
 			company: {},
-			list: {}
+			list: {},
+			anchorElement: null
 		};
 	}
-	componentDidUpdate() {
+	componentDidUpdate(prevProps) {
 		if (this.props.company) {
-			if (this.props.company.Members) {
-				if (this.isEmpty(this.state.list)) {
-					this.asd();
+			if (this.state.company !== prevProps.company) {
+				if (this.props.company.Members) {
+					// if (this.isEmpty(this.state.list) || ) {
+					this.fillMembersList();
+					// }
 				}
 			}
 		}
 	}
-	asd = () => {
+	fillMembersList = () => {
 		const { company } = this.props;
 		const tempList = {};
 		company.Groups.forEach(group => {
@@ -39,9 +44,18 @@ export default class CompanyMembers extends Component {
 			});
 		});
 
-		this.setState({ list: tempList }, () => {
+		this.setState({ list: tempList, company: company }, () => {
 			console.log(this.state.list);
 		});
+	};
+
+	handleClick = event => {
+		console.dir(event.currentTarget.getBoundingClientRect());
+		this.setState({ anchorElement: event.currentTarget });
+	};
+
+	handleClose = () => {
+		this.setState({ anchorElement: null });
 	};
 
 	isEmpty = obj => {
@@ -52,7 +66,7 @@ export default class CompanyMembers extends Component {
 	};
 
 	render() {
-		const { list } = this.state;
+		const { list, anchorElement } = this.state;
 		return (
 			<div className="company-members">
 				{!this.isEmpty(list) ? (
@@ -66,15 +80,36 @@ export default class CompanyMembers extends Component {
 									</div>
 									{list[group].map(item => {
 										return (
-											<ListItem key={item.key} button>
-												<Avatar alt="Profile picture" src={item.avatar} />
-												<ListItemText
-													primary={item.firstName + " " + item.lastName}
-													secondary={item.Roles.map(role => {
-														return role + " ";
-													})}
-												/>
-											</ListItem>
+											<Fragment key={item.key}>
+												<ListItem button onClick={this.handleClick}>
+													<Avatar alt="Profile picture" src={item.avatar} />
+													<ListItemText
+														primary={item.firstName + " " + item.lastName}
+														secondary={item.Roles.map(role => {
+															return role + " ";
+														})}
+													/>
+												</ListItem>
+												<Menu
+													id="simple-menu"
+													anchorEl={anchorElement}
+													open={Boolean(anchorElement)}
+													onClose={this.handleClose}
+													anchorReference="anchorPosition"
+													anchorPosition={{
+														top: anchorElement ? anchorElement.getBoundingClientRect().top + 50 : 0,
+														left: anchorElement ? anchorElement.getBoundingClientRect().left + 40 : 0
+													}}
+													transformOrigin={{
+														vertical: "top",
+														horizontal: "right"
+													}}
+												>
+													<MenuItem onClick={this.handleClose}>Profile</MenuItem>
+													<MenuItem onClick={this.handleClose}>Message</MenuItem>
+													<MenuItem onClick={this.handleClose}>Remove</MenuItem>
+												</Menu>
+											</Fragment>
 										);
 									})}
 								</div>
