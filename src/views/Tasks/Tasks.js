9179bcdb59ab0facbfb878185914b07f928b7a97
JackyThePro <naruto355@abv.bg>
2018-06-25 01:47:27 +0200

    Added poping tasks

--- src/views/Tasks/Tasks.js
+++ src/views/Tasks/Tasks.js
@@ -7,6 +7,10 @@ import { firestore } from "firebase";
 import CardContent from "@material-ui/core/CardContent/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Checkbox from "@material-ui/core/Checkbox";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 class Tasks extends Component {
 	static propTypes = {
@@ -20,7 +24,14 @@ class Tasks extends Component {
 		this.state = {
 			ref: firestore().collection("users"),
 			tasks: [],
-			checked: true
+			title: "",
+			CreatedAt: "",
+			deadline: "",
+			description: "",
+			createdBy: "",
+			companyId: "",
+			checked: true,
+			eventOpen: false
 		};
 	}
 
@@ -31,18 +42,48 @@ class Tasks extends Component {
 			snapshot.forEach(doc => {
 				let tempObj = {};
 				tempObj = doc.data();
-				console.log(tempObj);
 				tempObj["key"] = doc.id;
 				list.push(tempObj);
 			});
-			this.setState({ tasks: list });
+			this.setState({ eventOpen: true, tasks: list });
 		});
+	};
+
+	onCloseEventTile = (e) => {
+		e.stopPropagation();
+		this.setState({ eventOpen: false });
+	};
+
+	onSelectTask = (e,taskId) => {
+		e.stopPropagation();
+		firestore()
+			.collection("tasks")
+			.doc(taskId)
+			.get()
+			.then(info => {
+				const title = info.data().title;
+				const CreatedAt = info.data().createdAt.toDate().toDateString();
+				const deadline = info.data().deadline.toDate().toDateString();
+				const description = info.data().description;
+				const createdBy = info.data().createdBy;
+				const companyId = info.data().companyId;
+				this.setState({ eventOpen:true, title: title, CreatedAt: CreatedAt, deadline: deadline, description: description, createdBy: createdBy, companyId :companyId });
+			});
 	};
 
 	handleChange = (userId, key, boo) => {
 		firestore()
 			.collection("users")
 			.doc(userId)
+			.collection("tasks")
+			.doc(key)
+			.set(
+				{
+					isCompleted: !boo
+				},
+				{ merge: true }
+			);
+		firestore()
 			.collection("tasks")
 			.doc(key)
 			.set(
@@ -58,9 +99,8 @@ class Tasks extends Component {
 	}
 
 	render() {
-		const { tasks } = this.state;
+		const { tasks, title, CreatedAt, deadline, description, createdBy, companyId } = this.state;
 		const { numb } = this.props;
-		console.log(tasks);
 		let s = true;
 		return (
 			<div>
@@ -80,8 +120,28 @@ class Tasks extends Component {
 					{
 						return (
 							<Card key={index}>
-								<CardHeader title={task.title} />
-								<CardContent>Deadline: {task.deadline.toDate().toDateString(task.deadline)}</CardContent>
+								<CardHeader onClick={(e) => this.onSelectTask(e, task.key)} title={task.title}  />
+								{title?
+									<Dialog
+										open={this.state.eventOpen}
+										onClose={this.onCloseEventTile}
+										aria-labelledby="responsive-dialog-title"
+									>
+										<DialogTitle>{title}</DialogTitle>
+										<DialogContent>
+											<DialogContentText>
+												CreatedAt: {CreatedAt}
+											</DialogContentText>
+											<DialogContentText>
+												Deadline: {deadline}
+											</DialogContentText>
+											<DialogContentText>{description}</DialogContentText>
+											<DialogContentText>{createdBy}</DialogContentText>
+											<DialogContentText>{companyId}</DialogContentText>
+										</DialogContent>
+									</Dialog>
+									: (undefined)}
+								<CardContent>Deadline: {task.deadline.toDate().toDateString()}</CardContent>
 								<CardActions>
 									<Checkbox
 										checked={task.isCompleted}
